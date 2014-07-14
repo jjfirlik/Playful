9d1fc19d6504249813853b989650952b2fe5b792
jjfirlik <jacksonfirlik@Jacksons-MacBook-Pro-2.local>
2014-07-14 18:37:46 -0400

    Initial Commit

---
+++ Playful/JJFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JJFAppDelegate.h
+//  Playful
+//
+//  Created by Jackson Firlik on 7/14/14.
+//  Copyright (c) 2014 Jackson Firlik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Playful/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Playful
+//
+//  Created by Jackson Firlik on 7/14/14.
+//  Copyright (c) 2014 Jackson Firlik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JJFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JJFAppDelegate class]));
+    }
+}
